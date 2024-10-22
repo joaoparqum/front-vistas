0a90a345c9cb834d9ce200f72698306e96bdfa2e
João Paulo <costajoaopaulo113@gmail.com>
2024-10-22 11:02:23 -0300

    fix: change tech

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import 'ant-design-vue/dist/reset.css';
+  import Homescreen from './components/Homescreen.vue';
+</script>
+
+<template>
+  <Homescreen/>
+</template>
+
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // ...
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
+    vue(),
+  ],
+})
