0d4c04bedbac80ec87018dd37981832a0878ccff
João Paulo <costajoaopaulo113@gmail.com>
2024-11-26 08:45:31 -0300

    feat: add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,5 +16,6 @@ export default defineConfig({
       ],
     }),
     vue(),
+    VitePWA({ registerType: 'autoUpdate' }),
   ],
 })
