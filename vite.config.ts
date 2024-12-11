d7cf37507f9255a4102943d3ccccb0d2195b36e8
João Paulo <costajoaopaulo113@gmail.com>
2024-12-11 09:15:43 -0300

    fix: pwa manifest

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    // ...
     Components({
       resolvers: [
         AntDesignVueResolver({
@@ -16,6 +15,30 @@ export default defineConfig({
       ],
     }),
     vue(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Vistas Explodidas - Casa do Construtor | Irecê', // Nome completo do app
+        short_name: 'Vistas Explodidas', // Nome curto para atalhos
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff', // Cor de fundo
+        theme_color: '#0404cf', // Cor do tema (corrige o aviso)
+        icons: [
+          {
+            "src": "/web-app-manifest-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+          {
+            "src": "/web-app-manifest-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png",
+            "purpose": "maskable"
+          },
+        ],
+      },
+    }),
   ],
-})
+});
